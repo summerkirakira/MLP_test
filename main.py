2b83b86d60effcdbb17cad4cdb03f6b7951619f6
Summerkirakira <summerkirakira@gmail.com>
2023-10-25 00:34:36 +0100

    task 2 finished

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 from pathlib import Path
 from typing import Tuple
+from matplotlib import pyplot as plt
 
 data_path = Path('data/single')
 
@@ -23,14 +24,21 @@ def load_data() -> Tuple[np.ndarray, np.ndarray]:
         matrix_1.append(convert_text_to_array(file))
     for file in (data_path / '0').glob('*.txt'):
         matrix_0.append(convert_text_to_array(file))
-    return np.array(matrix_0), np.array(matrix_0)
+    return np.array(matrix_0), np.array(matrix_1)
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
 
 if __name__ == '__main__':
     data_set0, data_set1 = load_data()
 
-    train_data_set = np.vstack((data_set0[:3, :], data_set1[:3, :])).T
-    labels = [-1] * 3 + [1] * 3
+    train_data_set = np.vstack((data_set0[:4, :], data_set1[:4, :]))
+    test_data_set = np.vstack((data_set0[4:, :], data_set1[4:, :]))
+    test_labels = [-1] * 2 + [1] * 2
+
+    labels = [-1] * 4 + [1] * 4
 
     correct_response = np.array(labels).reshape(-1, 1)
 
@@ -41,15 +49,34 @@ if __name__ == '__main__':
     alpha = 0.025
 
     count = 0
+
     while True:
-        response = train_data_set.T @ weight_array + b
-        result = np.sign(response) - correct_response
+        response = train_data_set @ weight_array + b
+        result = correct_response - np.sign(response)
         if not np.any(result != 0):
             break
-        weight_array += alpha * train_data_set @ (correct_response - response)
-        b += np.sum(alpha * (correct_response - response))
+        weight_array += alpha * (train_data_set.T @ result)
+        b += np.sum(alpha * result)
         count += 1
         print(f"{count} try")
 
+    fig, axes = plt.subplots(3, 4, figsize=(8, 8))
 
-    print(weight_array)
+    for i in range(4):
+        axes[0, i].imshow(train_data_set[i].reshape((5, 5)), cmap='gray')
+        axes[0, i].axis('off')
+        axes[0, i].set_title(f"Label: {labels[i]}")
+        axes[1, i].imshow(train_data_set[i + 4].reshape((5, 5)), cmap='gray')
+        axes[1, i].axis('off')
+        axes[1, i].set_title(f"Label: {labels[i + 4]}")
+
+    for i in range(len(test_data_set)):
+        response = test_data_set[i]  @ weight_array + b
+        result = np.sign(response)
+        axes[2, i].imshow(test_data_set[i].reshape((5, 5)), cmap='gray')
+        axes[2, i].axis('off')
+        axes[2, i].set_title(f"Label: {test_labels[i]} / {result[0]}")
+
+    plt.show()
+
+    # print(weight_array)
---
+++ single_layer.py
@@ -0,0 +1,104 @@
+import numpy as np
+from pathlib import Path
+from matplotlib import pyplot as plt
+
+
+def load_idx3_ubyte(filename: Path):
+    with filename.open('rb') as f:
+        magic = int.from_bytes(f.read(4), 'big')
+        num = int.from_bytes(f.read(4), 'big')
+        rows = int.from_bytes(f.read(4), 'big')
+        cols = int.from_bytes(f.read(4), 'big')
+        images = np.empty((num, rows * cols), dtype=np.uint8)
+        for i in range(num):
+            for row in range(rows):
+                for col in range(cols):
+                    images[i][row * rows + col] = int.from_bytes(f.read(1), 'big')
+    return images
+
+
+def load_idx1_ubyte(filename: Path):
+    with filename.open('rb') as f:
+        magic = int.from_bytes(f.read(4), 'big')
+        num = int.from_bytes(f.read(4), 'big')
+        labels = np.empty((num,), dtype=np.uint8)
+        for i in range(num):
+            labels[i] = int.from_bytes(f.read(1), 'big')
+    return labels
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+if __name__ == '__main__':
+    tran_data_num = 6000
+    raw_train_images = load_idx3_ubyte(Path('data/handwritten_digits/train-images.idx3-ubyte'))
+    raw_train_labels = load_idx1_ubyte(Path('data/handwritten_digits/train-labels.idx1-ubyte'))
+
+    test_images = load_idx3_ubyte(Path('data/handwritten_digits/t10k-images.idx3-ubyte'))
+    test_labels = load_idx1_ubyte(Path('data/handwritten_digits/t10k-labels.idx1-ubyte'))
+
+    train_images = raw_train_images[:tran_data_num]
+    train_labels = raw_train_labels[:tran_data_num]
+
+    expect_output = np.zeros((tran_data_num, 10))
+    for i in range(tran_data_num):
+        expect_output[i][train_labels[i]] = 1
+
+    train_images = train_images.astype(float) / 255.0
+
+    iter_num = 1
+    alpha = 5e-5
+
+    np.random.seed(11451)
+
+    weight_array = np.random.rand(784, 10) * 0.01 - 0.005
+    b = np.random.rand(10, 1) * 0.01 - 0.005
+
+    while iter_num < 5000:
+        a_x = train_images @ weight_array + b.T
+        y_x = sigmoid(a_x)
+        delta = expect_output - y_x
+        # a = alpha * (delta.T @ y_x @ (1 - y_x).T @ train_images).T
+        weight_array += alpha * (train_images.T @ delta)
+        b += alpha * np.sum(delta, axis=0).reshape(10, 1)
+        if (iter_num % 1000) == 0:
+            print(f'iter_num: {iter_num} error: {np.sum(delta ** 2)}')
+        iter_num += 1
+
+    def predict(x):
+        output = x @ weight_array + b.T
+        return np.argmax(output, axis=1)
+
+    error_num = 0
+    error_list = []
+    for index, image in enumerate(test_images):
+        predict_number = predict(image)
+        if test_labels[index] != predict_number[0]:
+            error_num += 1
+            error_list.append({
+                'index': index,
+                'error': predict_number[0],
+                'expect': test_labels[index]
+            })
+            print(f'found: {predict_number[0]} expect: {test_labels[index]}')
+
+    print(f'error rate: {error_num / len(test_images)}')
+
+    fig, axes = plt.subplots(5, 5)
+
+    image_to_be_shown = np.random.choice(error_list, 25)
+    for i, ax in enumerate(axes.flat):
+        ax.imshow(test_images[image_to_be_shown[i]['index']].reshape((28, 28)), cmap='gray')
+        ax.axis('off')
+        ax.set_title(f"{image_to_be_shown[i]['error']} -> {image_to_be_shown[i]['expect']}")
+
+    plt.tight_layout()
+    plt.show()
+    # print(train_images.shape)
+
+    # plt.imshow(train_images[600].reshape((28, 28)), cmap='gray')
+    # plt.axis('off')
+    # plt.title(f"Label: {train_labels[600]}")
+    # plt.show()
